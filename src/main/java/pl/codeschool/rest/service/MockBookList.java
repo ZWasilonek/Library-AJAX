271e6de9b9342ea70b0bc19cc7613539248049a9
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-19 12:47:39 +0200

    MockBookList sample books added

--- src/main/java/pl/codeschool/rest/service/MockBookList.java
+++ src/main/java/pl/codeschool/rest/service/MockBookList.java
@@ -16,13 +16,13 @@ public class MockBookList {
                 .title("Thiniking in Java")
                 .isbn("9788324631766")
                 .author("Bruce Eckel")
-                .publisher("Helion")
+                .publisher("Prentice Hall")
                 .type("programming")
                 .build()
         );
         cList.add(
             new Book.BookBuilder().id(counter.getAndIncrement())
-                .title("Rusz glowa Java.")
+                .title("Rusz głową Java")
                 .isbn("9788324627738")
                 .author("Sierra Kathy, Bates Bert")
                 .publisher("Helion")
@@ -34,6 +34,24 @@ public class MockBookList {
                 .title("Java 2. Podstawy")
                 .isbn("9780130819338")
                 .author("Cay Horstmann, Gary Cornell")
+                .publisher("Nowy dzień")
+                .type("programming")
+                .build()
+        );
+        cList.add(
+            new Book.BookBuilder().id(counter.getAndIncrement())
+                .title("JavaScript - podstawy")
+                .isbn("8436023479338")
+                .author("Jan Kowalski")
+                .publisher("Replika")
+                .type("programming")
+                .build()
+        );
+        cList.add(
+            new Book.BookBuilder().id(counter.getAndIncrement())
+                .title("JavaScript i jQuery")
+                .isbn("2694739538325")
+                .author("Jon Duckett")
                 .publisher("Helion")
                 .type("programming")
                 .build()
