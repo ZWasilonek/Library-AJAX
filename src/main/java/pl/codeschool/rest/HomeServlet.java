9df0439fd7984f4b6dd6424b827ac051948876c9
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-19 12:01:40 +0200

    HomeServlet added

---
+++ src/main/java/pl/codeschool/rest/HomeServlet.java
@@ -0,0 +1,20 @@
+package pl.codeschool.rest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+
+        request.getRequestDispatcher("/index.html");
+    }
+
+}
